052504e30652f6c042bad8cc00848a06a66ece2e
Filali Abderaouf <filaliabderaouf54@gmail.com>
2023-04-17 07:51:41 +0100

    Pricing page update

--- client/src/components/Package card/PackageCard.tsx
+++ client/src/components/Package card/PackageCard.tsx
@@ -6,11 +6,9 @@ import SubscriptionCard from "../subsOfferCard/SubsOfferCard";
 
 function PackageCard() {
   return (
-    
     // first div just a text and description
     <>
-    
-    <NavBar/>
+      <NavBar />
       {/* // first div just a text and description */}
       <div className=" container  p-6 flex flex-col justify-start items-center mx-auto mt-10 mb-20">
         <h3>
@@ -24,7 +22,7 @@ function PackageCard() {
         </p>
 
         {/* // second div is the cards */}
-        <Pricing/>
+        <Pricing />
 
         {/* text of why we are diffrent */}
         <h1 className="  mt-5 text-[#565454] text-2xl font-extrabold ">
@@ -37,6 +35,17 @@ function PackageCard() {
             features than any other platform.
           </p>
         </div>
+        {/* Stats section */}
+        <img
+          className="p-10"
+          src="../../../assets/stats.png"
+          alt="statistics"
+        />
+        <img
+          className="p-10"
+          src="../../../assets/stats2.png"
+          alt="statistics"
+        />
       </div>
       <Footer />
     </>
