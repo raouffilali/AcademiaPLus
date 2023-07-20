4ba053b2492fc5f34b91f8bb4bd4a772b41f8d0b
SanaaArdjane <sanaaard@gmail.com>
2023-07-20 19:21:26 +0100

    primary school update

--- client/src/components/CustomCard/CustomCard.tsx
+++ client/src/components/CustomCard/CustomCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ArrowRightIcon, StarIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 import {
   Card,
   CardHeader,
@@ -7,8 +9,6 @@ import {
   Button,
   CardFooter,
 } from "@material-tailwind/react";
-import { ArrowLongRightIcon, ArrowRightIcon, StarIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom";
 
 interface CustomCardProps {
   imageSrc: string;
@@ -30,55 +30,11 @@ const CustomCard = ({
   const handleClick = () => {
     // Handle card click event here
   };
-
-  if (title === "الألعاب التعليمية" || title === "فروض و امتحانات") {
+  if (title === "مجموعة المراجعة") {
     return (
       <Link to={`/details/${year}/${subject}`} className="hover:bg-greenish">
-        <Card className="hover:bg-greenish flex-row w-full max-w-[48rem] border-dashed border" >
-          <CardHeader
-            shadow={false}
-            floated={false}
-            className="w-2/5 shrink-0 m-0 rounded-r-none"
-          >
-            <img
-              src={imageSrc}
-              alt="image"
-              className="w-full h-full object-cover bg-gray-200"
-            />
-            <Typography
-              variant="h6"
-              color="white"
-              className="absolute top-0 left-0 px-2 py-1 bg-emerald-500 rounded-tl-lg text-sm font-medium"
-            >
-              {year}
-            </Typography>
-          </CardHeader>
-          <CardBody>
-            <Typography
-              variant="h6"
-              className="text-emerald-500 uppercase mb-4"
-            >
-              {title}
-            </Typography>
-            <Typography className="text-gray-600 font-light text-sm mb-8">
-              {description}
-            </Typography>
-            <Button
-              variant="text"
-              className="flex text-emerald-500 hover:text-white items-center gap-2"
-            >
-              استكشف المزيد
-              <ArrowLongRightIcon strokeWidth={2} className="w-4 h-4" />
-            </Button>
-          </CardBody>
-        </Card>
-      </Link>
-    );
-  } else {
-    return (
-      <Link to={`/details/${year}/${subject}`} className="hover:bg-greenish">
-        <Card className="w-full max-w-[26rem] hover:bg-greenish border-dashed border">
-          <CardHeader
+        <Card className="w-full max-w-[26rem] h-full p-5   hover:bg-greenish border-dashed border">
+          <CardHeader 
             shadow={false}
             floated={false}
             color="blue-gray"
@@ -99,7 +55,67 @@ const CustomCard = ({
           </CardHeader>
           <CardBody>
             <div className="mb-3 flex items-center justify-between">
-              <Typography variant="h5" color="blue-gray" className="font-medium">
+              <Typography variant="h5" color="blue-gray" className="font-medium" style={{ fontFamily: "Tajawal", fontWeight: 600 }}>
+                {title}
+              </Typography>
+              <Typography
+                color="blue-gray"
+                className="flex items-center gap-1.5 font-normal"
+              >
+                <StarIcon className="-mt-0.5 h-5 w-5 text-yellow-500" />
+                {rating}
+              </Typography>
+            </div>
+            <Typography className="text-gray-600 font-light text-sm">
+              {description}
+            </Typography>
+          </CardBody>
+          <hr className="border border-dashed" />
+          <CardFooter className="pt-3">
+            <div className="flex  justify-between space-x-16">
+              {/* Group of User Avatars */}
+              <div className="flex -space-x-2">
+                <img src="/assets/instructor/user8.jpg" alt="User 1" className="w-8 h-8 rounded-full" />
+                <img src="/assets/instructor/user1.jpg" alt="User 2" className="w-8 h-8 rounded-full" />
+                <img src="/assets/instructor/user4.jpg" alt="User 3" className="w-8 h-8 rounded-full" />
+                <img src="/assets/instructor/user7.jpg" alt="User 3" className="w-8 h-8 rounded-full" />
+                <a className="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800" href="#">+99</a>
+              </div>
+              {/* Circular Button with White Arrow */}
+              <div className="bg-emerald-500 hover:bg-emerald-600  rounded-full p-2">
+                <ArrowRightIcon className="w-6 h-6 text-white" />
+              </div>
+            </div>
+          </CardFooter>
+        </Card>
+      </Link>
+    );
+  } else {
+  return (
+    <Link to={`/details/${year}/${subject}`} className="hover:bg-greenish">
+        <Card className=" w-full  max-w-[26rem] h-full p-4 hover:bg-greenish border-dashed border">
+          <CardHeader 
+            shadow={false}
+            floated={false}
+            color="blue-gray"
+            className="relative"
+          >
+            <img
+              src={imageSrc}
+              alt="image"
+              className="bg-blue-200 object-cover h-40 w-full rounded-t-xl"
+            />
+            <Typography
+              variant="h6"
+              color="white"
+              className="absolute top-0 right-0 px-2 py-1 bg-emerald-500 rounded-tr-lg text-sm font-medium"
+            >
+              {year}
+            </Typography>
+          </CardHeader>
+          <CardBody>
+            <div className="mb-3 flex items-center justify-between">
+              <Typography variant="h5" color="blue-gray" className=" font-medium"style={{ fontFamily: "Tajawal", fontWeight: 600 }}>
                 {title}
               </Typography>
               <Typography
@@ -125,7 +141,7 @@ const CustomCard = ({
           </CardFooter>
         </Card>
       </Link>
-    );
+  );
   }
 };
 
--- client/src/scenes/academicfieldPage/PrimarySchoolPage.tsx
+++ client/src/scenes/academicfieldPage/PrimarySchoolPage.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect, useRef } from "react";
 import Navbar from "../../components/navBar/NavBar";
 import PathPage from "../../components/PathPage/PathPage";
 import { motion } from "framer-motion";
+import "plyr/dist/plyr.css";
+import Plyr from "plyr";
+
 import {
   Card,
   CardHeader,
@@ -20,6 +23,7 @@ import {
   HomeIcon,
   TvIcon,
   FireIcon,
+  PlayIcon,
 } from "@heroicons/react/24/solid";
 import CustomCard from "../../components/CustomCard/CustomCard";
 import { Link } from "react-router-dom";
@@ -74,139 +78,176 @@ function PrimarySchoolPage() {
       document.body.removeEventListener("click", handleClickOutsideMenu);
     };
   }, []);
+  useEffect(() => {
+    const player = new Plyr("video");
+
+    return () => {
+      player.destroy();
+    };
+  }, []);
 
   return (
     <div className="font-tajawal">
       <Navbar />
       <PathPage />
-      <div  style={{ fontFamily: "Tajawal", fontWeight: 700 }}>
-      <div className="text-center ">
-        <div className="w-full mt-5 top-14 h-[680px] bg-blue-50 bg-[url(assets/bg/academic_bg.png)]">
-          <p
-            className="pt-16 font-medium text-3xl text-emerald-500"
-            style={{ fontFamily: "Tajawal", fontWeight: 700 }}
-          >
-            المرحلة الابتدائية
-          </p>
-          <p
-            className="text-gray-500 font-light"
-            style={{ fontFamily: "Amiri" }}
-          >
-            برامج الجيل الثاني
-          </p>
-          <div className="space-y-6 mx-[80px] mt-3">
-            <iframe
-              height="415"
-              src="https://www.youtube.com/embed/your-video-id"
-              title="Video"
-              className="w-2/3 rounded-2xl"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            ></iframe>
-            <button className="border-2 border-white rounded-2xl text-xl text-white font-bold p-2 px-16 hover:bg-emerald-500 bg-gradient-to-r from-emerald-600 to bg-emerald-300">
-              اشترك الان
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-full">
-        <div className="">
-          <p
-            className="pt-16 text-center font-medium text-3xl text-emerald-500"
-            style={{ fontFamily: "Tajawal", fontWeight: 700 }}
-          >
-            المحاور الدراسية للمرحلة الابتدائية
-          </p>
-          <div className="flex justify-center my-8">
-            <div className="flex justify-between w-full mx-[80px] bg-gray-50 border border-emerald-500 rounded-xl">
-              {years.map((year) => (
-                <button
-                  key={year}
-                  className={`py-2 md:px-12 hover:bg-emerald-500 rounded-s-xl rounded-e-xl duration-500 hover:text-white ${
-                    selectedYear === year ? "bg-emerald-500 text-white" : ""
-                  }`}
-                  style={{ fontFamily: "Tajawal", fontWeight: 600 }}
-                  onClick={() => handleYearChange(year)}
+      <div style={{ fontFamily: "Tajawal", fontWeight: 700 }}>
+        <div className="text-center ">
+          <div className="w-full mt-5 top-14 h-[680px] bg-blue-50 bg-[url(assets/bg/academic_bg.png)]">
+            <p
+              className="pt-16 font-medium text-3xl text-emerald-500"
+              style={{ fontFamily: "Tajawal", fontWeight: 700 }}
+            >
+              المرحلة الابتدائية
+            </p>
+            <p
+              className="text-gray-500 font-light"
+              style={{ fontFamily: "Amiri" }}
+            >
+              برامج الجيل الثاني
+            </p>
+            <div className="space-y-6 mx-[80px] mt-3">
+              <div className="w-2/3 mx-auto rounded-2xl border-2 border-dashed border-emerald-500 relative">
+                <video
+                  controls
+                  poster="/assets/academicfield/primaryThumbnai.jpeg"
+                  className="w-full h-full rounded-2xl"
                 >
-                  {year}
-                </button>
-              ))}
-            </div>
-          </div>
-          <div className="p-4 text-center">
-            <div className="relative inline-block" ref={menuRef}>
-              <button
-                className="flex items-center justify-between w-48 p-2 bg-white border-2 border-emerald-500 rounded-xl cursor-pointer"
-                onClick={() => setMenuOpen(!isMenuOpen)}
-              >
-                <span className="mr-4 text-emerald-500">
-                  {selectedSubject ? selectedSubject : "المادة"}
-                </span>
-                <svg
-                  className={`w-5 h-5 text-emerald-500 fill-current transform transition-transform ${
-                    isMenuOpen ? "rotate-180" : ""
-                  }`}
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M6 8l4 4 4-4z" />
-                </svg>
-              </button>
-              {isMenuOpen && (
-                <div className="z-10 absolute w-48 mt-2 bg-white border border-emerald-500 rounded-md shadow-lg">
-                  {subjects.map((subject) => (
-                    <button
-                      key={subject}
-                      className="w-full px-4 py-2 text-left hover:bg-emerald-500 hover:text-white"
-                      onClick={() => handleSubjectChange(subject)}
+                  <source src="/path/to/video.mp4" type="video/mp4" />
+                </video>
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <div className="bg-emerald-500 text-white rounded-full p-4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-8 h-8"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
                     >
-                      {subject}
-                    </button>
-                  ))}
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M15 12l-6-4v8l6-4z"
+                      />
+                    </svg>
+                  </div>
                 </div>
-              )}
+              </div>
+            </div>
+            <Link to="/subscriptionPackagesPage">
+              <button className="border-2 mt-5  border-white rounded-2xl text-xl text-white font-bold p-4 px-16 hover:bg-emerald-500 bg-gradient-to-r from-emerald-600 to bg-emerald-300">
+                اشترك الان
+              </button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="w-full">
+          <div className="">
+            <p
+              className="pt-16 text-center font-medium text-3xl text-emerald-500"
+              style={{ fontFamily: "Tajawal", fontWeight: 700 }}
+            >
+              المحاور الدراسية للمرحلة الابتدائية
+            </p>
+            <div className="flex justify-center my-8">
+              <div className="flex justify-between w-full mx-[80px] bg-gray-50 border border-emerald-500 rounded-xl">
+                {years.map((year) => (
+                  <button
+                    key={year}
+                    className={`py-2 md:px-12 hover:bg-emerald-500 rounded-s-xl rounded-e-xl duration-500 hover:text-white ${
+                      selectedYear === year ? "bg-emerald-500 text-white" : ""
+                    }`}
+                    style={{ fontFamily: "Tajawal", fontWeight: 600 }}
+                    onClick={() => handleYearChange(year)}
+                  >
+                    {year}
+                  </button>
+                ))}
+              </div>
+            </div>
+            <div className="p-4 text-center">
+              <div className="relative inline-block" ref={menuRef}>
+                <button
+                  className="flex items-center justify-between w-48 p-2 bg-white border-2 border-emerald-500 rounded-xl cursor-pointer"
+                  onClick={() => setMenuOpen(!isMenuOpen)}
+                >
+                  <span className="mr-4 text-emerald-500">
+                    {selectedSubject ? selectedSubject : "المادة"}
+                  </span>
+                  <svg
+                    className={`w-5 h-5 text-emerald-500 fill-current transform transition-transform ${
+                      isMenuOpen ? "rotate-180" : ""
+                    }`}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M6 8l4 4 4-4z" />
+                  </svg>
+                </button>
+                {isMenuOpen && (
+                  <div className="z-10 absolute w-48 mt-2 bg-white border border-emerald-500 rounded-md shadow-lg">
+                    {subjects.map((subject) => (
+                      <button
+                        key={subject}
+                        className="w-full px-4 py-2 text-left hover:bg-emerald-500 hover:text-white"
+                        onClick={() => handleSubjectChange(subject)}
+                      >
+                        {subject}
+                      </button>
+                    ))}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-2 gap-8 md:grid-cols-3 mx-[80px] ">
-        {/* Card 2 */}
-        <Link to="/card2">
-          <CustomCard
-            imageSrc="/assets/academicfield/molakhasat.jpg"
-            title="الملخصات المكتوبة"
-            rating="4.5"
-            description="Enter a description for الملخصات المكتوبة."
-            year={selectedYear}
-            subject={selectedSubject}
-          />
-        </Link>
+        <div className="grid grid-cols-1 gap-7 md:grid-cols-3 mx-[80px] ">
+          {/* Card 2 */}
+          <Link to="/card2">
+            <CustomCard
+              imageSrc="/assets/academicfield/molakhasat.jpg"
+              title="الملخصات المكتوبة"
+              rating="4.5"
+              description="Enter a description for الملخصات المكتوبة."
+              year={selectedYear}
+              subject={selectedSubject}
+            />
+          </Link>
 
-        {/* Card 3 */}
-        <Link to="/card3">
+          {/* Card 3 */}
+          <Link to="/card3">
+            <CustomCard
+              imageSrc="/assets/academicfield/videoCourse.jpg"
+              title="ملخصات بالفيديو"
+              rating="4.0"
+              description="Enter a description for ملخصات بالفيديو."
+              year={selectedYear}
+              subject={selectedSubject}
+            />
+          </Link>
+          {/* Card 1 */}
+          <Link to="/card1">
+            <CustomCard
+              imageSrc="/assets/academicfield/dawra.png"
+              title="الدورات التعليمية"
+              rating="5.0"
+              description="Enter a description for الدورات التعليمية."
+              year={selectedYear}
+              subject={selectedSubject}
+            />
+          </Link>
+          {/* Card for "مجموعة المراجعة" */}
           <CustomCard
-            imageSrc="/assets/academicfield/videoCourse.jpg"
-            title="ملخصات بالفيديو"
-            rating="4.0"
-            description="Enter a description for ملخصات بالفيديو."
+            imageSrc="/assets/academicfield/revision.jpg"
+            title="مجموعة المراجعة"
+            rating="4.7"
+            description="Enter a description for مجموعة المراجعة."
             year={selectedYear}
             subject={selectedSubject}
           />
-        </Link>
-        {/* Card 1 */}
-        <Link to="/card1">
-          <CustomCard
-            imageSrc="/assets/academicfield/dawra.png"
-            title="الدورات التعليمية"
-            rating="5.0"
-            description="Enter a description for الدورات التعليمية."
-            year={selectedYear}
-            subject={selectedSubject}
-          />
-        </Link>
+          {/* Card 4 */}
 
-        {/* Card 4 */}
-        <div className="col-span-1">
           <Link to="/card5">
             <CustomCard
               imageSrc="/assets/academicfield/games.jpg"
@@ -217,9 +258,9 @@ function PrimarySchoolPage() {
               subject={selectedSubject}
             />
           </Link>
-        </div>
-        {/* Card 5 */}
-        <div className="col-span-2 mb-5 ">
+
+          {/* Card 5 */}
+
           <Link to="/card4">
             <CustomCard
               imageSrc="/assets/academicfield/exams.jpg"
@@ -231,14 +272,10 @@ function PrimarySchoolPage() {
             />
           </Link>
         </div>
+        <div className="mt-24 ">
+          <VirtualLabSection />
+        </div>
       </div>
-      <div className="mt-24 ">
-
-      <VirtualLabSection/>
-      </div>
-
-      </div>
-      
 
       <Footer />
     </div>
