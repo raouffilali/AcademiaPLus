cdbe7962dff8864f0c52de9367fc206738f72d04
Abderraouf FILALI <filaliabderaouf54@gmail.com>
2023-09-03 19:42:15 +0100

    adding middlewares for uploading files pictures videos

---
+++ server/src/middleware/uploadMultipleVideosMiddleware.ts
@@ -0,0 +1,32 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../public/uploads/multiple_videos"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const uploadMultipleVideos = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 100, // 100MB (adjust as needed)
+  },
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /mp4|avi|mov/; // Add more video extensions as needed
+    const extName = fileTypes.test(
+      file.originalname.split(".")[file.originalname.split(".").length - 1]
+    );
+    const mimeType = fileTypes.test(file.mimetype);
+    if (extName && mimeType) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only support .mp4, .avi, .mov"));
+    }
+  },
+});
+
+export default uploadMultipleVideos;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,15 @@ import mongoose from "mongoose";
 import app from "./app";
 import env from "./utils/validateEnv";
 
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as mongoose.ConnectOptions;
+
 const port = env.PORT || 6000;
 
 mongoose
-  .connect(env.MONGO_CONNECTION_STRING)
+  .connect(env.MONGO_CONNECTION_STRING, options)
   .then(() => {
     console.log("Mongoose connected");
     app.listen(port, () => {
