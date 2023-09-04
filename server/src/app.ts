f1fd2414b7d4af3e10f94b66fd777231b2a20934
Abderraouf FILALI <filaliabderaouf54@gmail.com>
2023-09-04 16:28:05 +0100

    adding log4js and Errors handler

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,10 @@ import createHttpError, { isHttpError } from "http-errors";
 import morgan from "morgan";
 import env from "./utils/validateEnv";
 import blogsRouter from "./routes/BlogsRoute";
+import {
+  errorResponseHandler,
+  invalidPathHandler,
+} from "./middleware/errorHandler";
 
 const app = express();
 
@@ -14,37 +18,29 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
-// app.use(session({
-//     secret: env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         maxAge: 60 * 60 * 1000,
-//     },
-//     rolling: true,
-//     store: MongoStore.create({
-//         mongoUrl: env.MONGO_CONNECTION_STRING
-//     }),
-// }));
 
-app.use("/api/blogs", blogsRouter);
-
-
-
-app.use((req, res, next) => {
-    next(createHttpError(404, "Endpoint not found"));
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
-    console.error(error);
-    let errorMessage = "An unknown error occurred";
-    let statusCode = 500;
-    if (isHttpError(error)) {
-        statusCode = error.status;
-        errorMessage = error.message;
-    }
-    res.status(statusCode).json({ error: errorMessage });
-});
+app.use(invalidPathHandler);
+app.use(errorResponseHandler);
+
 
 export default app;
+
+// app.use((req, res, next) => {
+//     next(createHttpError(404, "Endpoint not found"));
+// });
+
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+//     console.error(error);
+//     let errorMessage = "An unknown error occurred";
+//     let statusCode = 500;
+//     if (isHttpError(error)) {
+//         statusCode = error.status;
+//         errorMessage = error.message;
+//     }
+//     res.status(statusCode).json({ error: errorMessage });
+// });
